b41484373b7a688a1152e0ebe47f7dcebe7f8eb1
LewisJassy <jassylewis70@gmail.com>
2024-01-20 06:47:10 +0300

    big O notation for burble algorthm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "sort.h"
+#include <stdbool.h>
+
+void swapped(int *x, int *y)
+{
+    int t = *x;
+    *x = *y;
+    *y = t;
+}
+void insertion_sort(listint_t **list)
+{
+    
+}
