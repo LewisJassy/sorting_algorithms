fa762d0fdcc535ced31d57e15bc835c1bf9cb92d
LewisJassy <jassylewis70@gmail.com>
2024-01-20 07:06:17 +0300

    update to correct failling checks

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,34 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include "sort.h"
-/*
-* This function is used to print an array of integers in ascending order.  It takes as
-* input the number of elements in the array, a pointer to the first element of the array,
-* and an optional message that will be printed before the array.
-*/
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
 
+/**
+ * bubble_sort - the worst sorting algorithm
+ * @array: array to be sorted in place
+ * @size: size of array
+ */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    bool swapped;
-    for (i = 0; i < size - 1; i++) {
-        swapped = false;
-        for (j = 0; j < size - 1 - i; j++) {
-            if (array[j] > array[j + 1]) {
-                swap(&array[j], &array[j +1]);
-                swapped = true;
-            }
-        }
-        /* If no two elements were swapped by inner loop, then the array is sorted */
-        if (!swapped)
-            break;
-    }
-}
+	size_t i, j;
+	int max;
 
+	for (i = size; i > 1; i--)
+	{
+		for (j = 0; j < i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				max = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = max;
+				print_array(array, size);
+			}
+		}
+	}
+}
